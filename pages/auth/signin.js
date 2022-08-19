abd23337fc794b7d0129956a2ce0a89a3b5cd56d
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-19 07:42:45 +0530

    Added sign in form

---
+++ pages/auth/signin.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signin',
+    method: 'post',
+    body: { email, password },
+    onSuccess: () => Router.push('/'),
+  });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    doRequest();
+  };
+
+  return (
+    <form onSubmit={onSubmit} className="container mt-5">
+      <h1 className="mb-3">Sign In</h1>
+
+      <div className="form-group mb-3">
+        <label>Email Address</label>
+        <input
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <div className="form-group mb-3">
+        <label>Password</label>
+        <input
+          className="form-control"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      {errors}
+
+      <button className="btn btn-primary">Sign In</button>
+    </form>
+  );
+}
