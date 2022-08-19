0d6e70a4b2a89f23e7006b7a4b5ebd215d580db1
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-19 08:29:16 +0530

    Added getInitial props in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps, currentUser }) {
+  return (
+    <>
+      <h1>Header {currentUser?.email}</h1>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+MyApp.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,9 @@ const LandingPage = ({ currentUser }) => {
 
 LandingPage.getInitialProps = async (context) => {
   const client = buildClient(context);
+  console.log('Landing page');
   const { data } = await client.get('/api/users/currentuser');
+
   return data;
 };
-
 export default LandingPage;
