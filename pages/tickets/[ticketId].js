db9b7f5f0eb2d462c104e9de900459f1b2c7e17c
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-25 11:00:52 +0530

    Creating an order

---
+++ pages/tickets/[ticketId].js
@@ -0,0 +1,34 @@
+import React from 'react';
+import useRequest from '../../hooks/use-request';
+
+export default function TicketDetail({ ticket }) {
+  const { doRequest, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: {
+      ticketId: ticket.id,
+    },
+    onSuccess: (order) => console.log(order),
+  });
+
+  const onBuyClick = () => {
+    doRequest();
+  };
+
+  return (
+    <div>
+      <h1>{ticket.title}</h1>
+      <h4>Price: ${ticket.price}</h4>
+      {errors}
+      <button className="btn btn-primary" onClick={onBuyClick}>
+        Buy Now
+      </button>
+    </div>
+  );
+}
+
+TicketDetail.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+  return { ticket: data };
+};
