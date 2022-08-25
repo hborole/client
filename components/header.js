0be931d109ffa16b43aa15ae86b7d2d7ff34e0e5
Himanshu Borole <himanshu.borole@gmail.com>
2022-08-25 11:48:27 +0530

    Adding links in header

--- components/header.js
+++ components/header.js
@@ -4,6 +4,8 @@ export default function Header({ currentUser }) {
   const links = [
     !currentUser && { label: 'Sign Up', href: '/auth/signup' },
     !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'My Orders', href: '/orders' },
+    currentUser && { label: 'Sell tickets', href: '/tickets/new' },
     currentUser && { label: 'Sign Out', href: '/auth/signout' },
   ]
     .filter((linkConfig) => linkConfig)
